eb6a496125dda32f4b779da7d9e6baeff72c85a3
willi <willi@DESKTOP-689IO0P>
2021-09-16 17:36:47 -0300

    v2-o1

---
+++ src/br/edu/fateczl/WillianMeirelles/pilhaint/No.java
@@ -0,0 +1,8 @@
+package br.edu.fateczl.WillianMeirelles.pilhaint;
+
+public class No {
+
+	int dado;
+	No proximo;
+	
+}
---
+++ src/br/edu/fateczl/WillianMeirelles/pilhastring/No.java
@@ -0,0 +1,10 @@
+package br.edu.fateczl.WillianMeirelles.pilhastring;
+
+import br.edu.fateczl.WillianMeirelles.pilhastring.No;
+
+public class No {
+
+	String dado;
+	No proximo;
+	
+}
